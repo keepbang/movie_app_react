b84ffbc71a19e21cf0ebe56ac868cd73c975d9e8
Gibyoung <38740455+keepbang@users.noreply.github.com>
2020-09-09 16:57:43 +0900

    #3.1

--- src/App.js
+++ src/App.js
@@ -2,25 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+  state = {
+    count: 0
+  };
 
   add = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-    this.resultPrint();
+    this.setState((cur) => ({
+      count: cur.count + 1
+    }));
   };
 
   minus = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-    this.resultPrint();
+    this.setState((cur) => ({
+      count: cur.count - 1
+    }));
   };
 
   render() {
