5fd78a9a7306fd54612039e3ea892dfa988f60a2
keepbang <rlqud1125@gmail.com>
2020-09-14 22:39:26 +0900

    Updates

---
+++ precache-manifest.b3db6b6e3e58c0f9847f0cd433ea6978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c197df0515cc6b6607020b410fe68c",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "6aaa47cbbb5a04b71918",
+    "url": "/movie_app_react/static/css/main.7f8d32f5.chunk.css"
+  },
+  {
+    "revision": "6f60b538772c73845829",
+    "url": "/movie_app_react/static/js/2.72bea2a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react/static/js/2.72bea2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aaa47cbbb5a04b71918",
+    "url": "/movie_app_react/static/js/main.70b7a270.chunk.js"
+  },
+  {
+    "revision": "e772194c85f13f1bd6a9",
+    "url": "/movie_app_react/static/js/runtime-main.b1700b33.js"
+  }
+]);
