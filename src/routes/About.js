77e00b5144a827c2bdc4db8b9d5091e393e5674c
keepbang <rlqud1125@naver.com>
2020-09-18 16:08:45 +0900

    detail page

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,7 +5,12 @@ function About(props){
     console.log(props);
     return (
         <div className="about_main">
-            <span>About this page</span>
+            <h2>React Movie App</h2>
+            <br/>
+            <a href="https://github.com/keepbang/react-project-map">
+            <img width="100px" src="https://cdn.icon-icons.com/icons2/535/PNG/512/Github-Icon_icon-icons.com_52888.png"/>
+            </a>
+            <br/>
         </div>
     )
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,15 +1,51 @@
-import React,{useEffect} from 'react'
+import React from "react";
+import "./Detail.scss";
 
-function Detail(props){
-    useEffect(() => {
-        const {history,location} = props;
-        if(location.state === undefined){
+
+class MovieDetail extends React.Component{
+    componentDidMount() {
+        const {location, history } = this.props;
+        console.log(location.state);
+        if (location.state === undefined) {
             history.push("/");
         }
-    }, [])
-    return (
-        <span>Hello</span>
-    )
+    }
+    com
+    render(){
+        const {location} = this.props;      
+        
+        if (location.state) {
+            const {
+                location: {
+                    state: {
+                        genres, 
+                        poster,
+                        summary,
+                        title,
+                        year
+                    }}} = this.props;
+
+            return (
+                <div className="movieDetail__container">
+                    <div className="movie__poster">
+                        <img src={poster} alt={title} />
+                    </div>
+                    <div className="movie__data">
+                        <h3 className="movie__title">{title}</h3>
+                        <h5 className="movie__year">{year}</h5>
+                        <ul className="movie__genres">
+                            {genres.map((genre, idx) => (<li key={idx} className="genres__li">{genre}</li>))}
+                        </ul>
+                        <p className="movie__summary">{summary}</p>
+                    </div>
+                </div>
+                )
+        }
+        else{
+            return null;
+        }
+    }
 }
 
-export default Detail;
+
+export default MovieDetail;
